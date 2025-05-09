37aace0c05ff0552059b7c3fc5ff7ed19b7b885e
hyuk-dev <hyuk.development@gmail.com>
2025-05-09 11:27:33 +0900

    fix: 오타 수정

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,6 +5,6 @@ describe("GET /", () => {
   it("기본 경로(/)는 200 상태코드와 함께 환영메시지를 응답해야합니다.", async () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe("Hello World form Express!");
+    expect(response.text).toBe("Hello World from Express!");
   });
 });
